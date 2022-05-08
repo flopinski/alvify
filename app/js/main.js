c30f91c51603e807a01defc08895890e43665ece
flopinski <nurlan410@aol.com>
2022-05-08 23:54:18 +0300

    added shop page

--- app/js/main.js
+++ app/js/main.js
@@ -1,18 +1,46 @@
-$(function(){
-  
-  $('.top-slider__inner').slick({
+$(function () {
+  $(".shop-content__filter-btn").on("click", function () {
+    $('.shop-content__filter-btn').removeClass('shop-content__filter-btn--active');
+    $(this).addClass('shop-content__filter-btn--active');
+
+  });
+
+  $('.button-list').on('click', function () {
+    $('.product-item').addClass('product-item--list')
+  });
+
+  $('.button-grid').on('click', function () {
+    $('.product-item').removeClass('product-item--list')
+  });
+
+  $(".select-style").styler();
+
+  $(".filter-price__input").ionRangeSlider({
+    type: "double",
+    prefix: "$",
+    onStart: function (data) {
+      $(".filter-price__from").text(data.from);
+      $(".filter-price__to").text(data.to);
+    },
+    onChange: function (data) {
+      $(".filter-price__from").text(data.from);
+      $(".filter-price__to").text(data.to);
+    },
+  });
+
+  $(".top-slider__inner").slick({
     dots: true,
     arrows: false,
     fade: true,
     autoplay: true,
-    autoplaySpeed: 2000
+    autoplaySpeed: 2000,
   });
 
   $(".star").rateYo({
     starWidth: "17px",
     normalFill: "#ccccce",
     ratedFill: "#ffc35b",
-    readOnly: true
+    readOnly: true,
   });
 
   function getTimeRemaining(endtime) {
@@ -32,7 +60,7 @@ $(function(){
   }
 
   function initializeClock(id, endtime) {
-    const clock = document.querySelector('.promo__clock');
+    const clock = document.querySelector(".promo__clock");
     const daysSpan = clock.querySelector(".promo__days");
     const hoursSpan = clock.querySelector(".promo__hours");
     const minutesSpan = clock.querySelector(".promo__minutes");
@@ -55,15 +83,6 @@ $(function(){
     const timeinterval = setInterval(updateClock, 1000);
   }
 
-  const deadline = $('.promo__clock').attr('data-time');
+  const deadline = $(".promo__clock").attr("data-time");
   initializeClock("promo__clock", deadline);
-
-
-
-
-
-
-
-
-
 });
