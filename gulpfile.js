0dd0b5603674c68438de5505bf5594045c4f9ba2
flopinski <103384762+flopinski@users.noreply.github.com>
2022-06-04 11:12:50 +0300

    add links

--- gulpfile.js
+++ gulpfile.js
@@ -75,7 +75,7 @@ function images() {
 }
 
 function build() {
-  return src(["app/**/*.html", "app/css/style.min.css", "style.min.css", "app/js/main.min.js"], {
+  return src(["app/**/*.html", "app/css/style.min.css", "app/js/main.min.js"], {
     base: "app",
   }).pipe(dest("dist"));
 }
